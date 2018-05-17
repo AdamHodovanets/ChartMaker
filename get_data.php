b24f92c0b8f35e7ffa1e9cb1a43511590d1e82e8
Adam Hodovanets <adamhodovanets@gmail.com>
2018-05-17 20:41:17 +0300

    Chart changed to timeline
    Added Google Charts library.

--- get_data.php
+++ get_data.php
@@ -1,3 +1,14 @@
 <?php
-$time = 10;
-$cardName = 'testCard';
+function getData()
+{
+    for($i=1;$i<10;$i++)
+    {
+        $data[] =[
+            'x' => "new Date(2018, 5,$i)",
+            'y' => $i
+        ];
+    }
+
+    $result = json_encode($data);
+    return $result;
+}
--- index.php
+++ index.php
@@ -4,12 +4,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Admiagram</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" type="text/css" href="https://rawgit.com/gionkunz/chartist-js/master/dist/chartist.min.css">
 
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="https://rawgit.com/gionkunz/chartist-js/master/dist/chartist.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="js/script.js"></script>
 </head>
 <body>
